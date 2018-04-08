d9853369ba02b138cca0622ac00f2b0df217ec27
YueHub <li_chuan_yue@foxmail.com>
2018-04-08 18:49:59 +0800

    将部分代码移至语义解析模块

--- src/cn/lcy/ontology/query/Main.java
+++ src/cn/lcy/ontology/query/Main.java
@@ -1,18 +1,19 @@
 package cn.lcy.ontology.query;
 
+import cn.lcy.ontology.query.dao.QueryDAOI;
+import cn.lcy.ontology.query.dao.QueryDAOImpl;
 import cn.lcy.ontology.query.sem.model.QueryResult;
-import cn.lcy.ontology.query.service.QueryServiceI;
-import cn.lcy.ontology.query.service.QueryServiceImpl;
 
 public class Main {
 
   public static void main(String[] args) {
-    QueryServiceI queryService = QueryServiceImpl.getInstance();
+    QueryDAOI queryDAO = QueryDAOImpl.getInstance();
 
     String query = "SELECT ?导演姓名 WHERE { mymo:489e1493d8b34285b5a24017e574c0f5 mymo:有导演 ?导演.\n"
       + "?导演 mymo:有中文名 ?导演姓名.}";
-    QueryResult queryResult = queryService.queryOntology(query);
+    QueryResult queryResult = queryDAO.queryOntology(query);
     System.out.println(queryResult.getAnswers().get(0).getContent());
+    System.out.println(queryDAO.querySameIndividual("c1eff05c87504975b2820960ee25b2d7"));
   }
 
 }
