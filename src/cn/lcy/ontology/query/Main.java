c4fbe7c93e0bec21a0166d6c71c84977f9d2ff8d
YueHub <li_chuan_yue@foxmail.com>
2020-02-27 22:39:05 +0800

    修正缩进

--- src/cn/lcy/ontology/query/Main.java
+++ src/cn/lcy/ontology/query/Main.java
@@ -6,14 +6,14 @@ import cn.lcy.ontology.query.sem.model.QueryResult;
 
 public class Main {
 
-  public static void main(String[] args) {
-    QueryDAOI queryDAO = QueryDAOImpl.getInstance();
+    public static void main(String[] args) {
+        QueryDAOI queryDAO = QueryDAOImpl.getInstance();
 
-    String query = "SELECT ?导演姓名 WHERE { mymo:489e1493d8b34285b5a24017e574c0f5 mymo:有导演 ?导演.\n"
-      + "?导演 mymo:有中文名 ?导演姓名.}";
-    QueryResult queryResult = queryDAO.queryOntology(query);
-    System.out.println(queryResult.getAnswers().get(0).getContent());
-    System.out.println(queryDAO.querySameIndividual("c1eff05c87504975b2820960ee25b2d7"));
-  }
+        String query = "SELECT ?导演姓名 WHERE { mymo:489e1493d8b34285b5a24017e574c0f5 mymo:有导演 ?导演.\n"
+                + "?导演 mymo:有中文名 ?导演姓名.}";
+        QueryResult queryResult = queryDAO.queryOntology(query);
+        System.out.println(queryResult.getAnswers().get(0).getContent());
+        System.out.println(queryDAO.querySameIndividual("c1eff05c87504975b2820960ee25b2d7"));
+    }
 
 }
--- src/cn/lcy/ontology/query/config/Config.java
+++ src/cn/lcy/ontology/query/config/Config.java
@@ -1,68 +1,66 @@
 package cn.lcy.ontology.query.config;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Properties;
-
+import cn.lcy.ontology.query.util.Configuration;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.util.FileManager;
 
-import cn.lcy.ontology.query.util.Configuration;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Properties;
 
 public class Config {
-	
-	/**
-	 * 配置文件名
-	 */
-	public static final String PROPERTY_FILE = "answer-ontology-query.properties";
-	
-	/**
-	 * 配置
-	 */
-	public static Properties properties;
+
+    /**
+     * 配置文件名
+     */
+    public static final String PROPERTY_FILE = "answer-ontology-query.properties";
+
+    /**
+     * 配置
+     */
+    public static Properties properties;
 
     /**
      * 是否使用默认本体数据
      */
-	public static String defaultDataSource;
-	
-	/**
-	 * 本体标识
-	 */
-	public static String pizzaNs;
-	
-	/**
-	 * 根路径
-	 */
-	public static String rootPath;
-	
-	/**
-	 * 本体文件路径
-	 */
-	public static String ontologyPath;
+    public static String defaultDataSource;
 
-	
-	public static OntModel model;
-	
-	public static Model loadModel;
-	
-	/**
-	 * 读取配置
-	 */
-	static {
-		try {
-			properties = Configuration.propertiesLoader(PROPERTY_FILE);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    /**
+     * 本体标识
+     */
+    public static String pizzaNs;
+
+    /**
+     * 根路径
+     */
+    public static String rootPath;
+
+    /**
+     * 本体文件路径
+     */
+    public static String ontologyPath;
+
+
+    public static OntModel model;
+
+    public static Model loadModel;
+
+    /**
+     * 读取配置
+     */
+    static {
+        try {
+            properties = Configuration.propertiesLoader(PROPERTY_FILE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         pizzaNs = properties.getProperty("pizzaNs").toString();
         defaultDataSource = properties.getProperty("defaultDataSource").toString();
 
-		if ("true".equals(defaultDataSource)) { // 使用默认数据源
+        if ("true".equals(defaultDataSource)) { // 使用默认数据源
             try {
                 ontologyPath = Thread.currentThread().getContextClassLoader().getResource("").toURI().getPath().toString() + "data/Ontologies/Answer_Ontology_V2.owl";
             } catch (URISyntaxException e) {
@@ -74,11 +72,12 @@ public class Config {
         }
         model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
         loadModel = FileManager.get().readModel(model, ontologyPath);
-	}
-	
-	/**
-	 * 配置类 不需要生成实例
-	 */
-	private Config() {}
+    }
+
+    /**
+     * 配置类 不需要生成实例
+     */
+    private Config() {
+    }
 
 }
